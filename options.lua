916b5d7821daf12efbfdff9b3186b483989d8e79
Kanata Miyahana <afiri.bupa@gmail.com>
2023-06-11 18:39:49 +0900

    update cofig

--- options.lua
+++ options.lua
@@ -2,10 +2,10 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    spell = true, -- sets vim.opt.spell
+    signcolumn = "yes", -- sets vim.opt.signcolumn to yes
     wrap = false, -- sets vim.opt.wrap
   },
   g = {
